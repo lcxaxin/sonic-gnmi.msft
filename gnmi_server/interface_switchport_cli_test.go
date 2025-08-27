a7687e0c4c003372f447da48960f496c55af4e8c
lcxaxin <chenxinli@chenxinli-dev-vm.d4y3nv5wwgfelhhopdxv1tqjld.dx.internal.cloudapp.net>
2025-08-27 12:54:55 +0000

    Add more UT

--- gnmi_server/interface_switchport_cli_test.go
+++ gnmi_server/interface_switchport_cli_test.go
@@ -6,6 +6,8 @@ package gnmi
 
 import (
 	"crypto/tls"
+	"fmt"
+	"os"
 	"testing"
 	"time"
 
@@ -15,6 +17,9 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+
+	show_client "github.com/sonic-net/sonic-gnmi/show_client"
+	sdc "github.com/sonic-net/sonic-gnmi/sonic_data_client"
 )
 
 // Test SHOW interface switchport config
@@ -40,7 +45,7 @@ func TestGetShowInterfaceSwitchportConfig(t *testing.T) {
     portsFileName := "../testdata/PORTS_SWITCHPORT.txt"
     vlanMemberFileName := "../testdata/VLAN_MEMBER_SWITCHPORT.txt"
 
-    expectedConfig := `[{"Interface":"Ethernet0","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet1","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet2","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet3","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet4","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet5","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet6","Mode":"routed","Tagged":"","Untagged":""},{"Interface":"Ethernet7","Mode":"routed","Tagged":"","Untagged":""}]`
+    expectedConfig := `[{"Interface":"Ethernet0","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet1","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet2","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet3","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet4","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet5","Mode":"trunk","Tagged":"","Untagged":"1000"},{"Interface":"Ethernet6","Mode":"trunk","Tagged":"1000","Untagged":""},{"Interface":"Ethernet7","Mode":"routed","Tagged":"","Untagged":""}]`
 
     tests := []struct {
         desc        string
@@ -51,6 +56,8 @@ func TestGetShowInterfaceSwitchportConfig(t *testing.T) {
         valTest     bool
         mockSleep   bool
         testInit    func()
+        mockPatch   func() *gomonkey.Patches // optional: apply extra patches; returned patches will be Reset
+        teardown    func()                   // optional: run after patches.Reset(), e.g. restore env
     }{
         {
             desc:       "query SHOW interface switchport config NO DATA",
@@ -96,22 +103,139 @@ func TestGetShowInterfaceSwitchportConfig(t *testing.T) {
             valTest:     true,
             // reuse dataset loaded by previous test (no testInit)
         },
+        {
+            desc:       "query SHOW interface switchport config - GetMapFromQueries returns error",
+            pathTarget: "SHOW",
+            textPbPath: `
+                elem: <name: "interface" >
+                elem: <name: "switchport" >
+                elem: <name: "config" >
+            `,
+            wantRetCode: codes.NotFound, // server wraps getter errors as NotFound
+            valTest:     false,
+            testInit: func() {
+                FlushDataSet(t, ConfigDbNum)
+            },
+            mockPatch: func() *gomonkey.Patches {
+                // inject GetMapFromQueries failure when table == "PORT"
+                return gomonkey.ApplyFunc(show_client.GetMapFromQueries, func(queries [][]string) (map[string]interface{}, error) {
+                    if len(queries) > 0 && len(queries[0]) > 1 && queries[0][1] == "PORT" {
+                        return nil, fmt.Errorf("injected GetMapFromQueries failure")
+                    }
+                    // otherwise return empty but successful map
+                    return map[string]interface{}{}, nil
+                })
+            },
+        },
+        {
+            desc:       "query SHOW interface switchport config - alias display (SONIC_CLI_IFACE_MODE=alias)",
+            pathTarget: "SHOW",
+            textPbPath: `
+                elem: <name: "interface" >
+                elem: <name: "switchport" >
+                elem: <name: "config" >
+            `,
+            wantRetCode: codes.OK,
+            valTest:     false,
+            testInit: func() {
+                FlushDataSet(t, ConfigDbNum)
+                AddDataSet(t, ConfigDbNum, portsFileName)
+                AddDataSet(t, ConfigDbNum, vlanMemberFileName)
+            },
+            mockPatch: func() *gomonkey.Patches {
+                // set alias mode and provide PortToAliasNameMap -> safe and isolated via patch
+                os.Setenv(show_client.SonicCliIfaceMode, "alias")
+                p := gomonkey.ApplyFunc(sdc.PortToAliasNameMap, func() map[string]string {
+                    return map[string]string{
+                        "Ethernet0": "etp0",
+                        "Ethernet1": "etp1",
+                    }
+                })
+                // restore env after test via teardown
+                // but teardown will be invoked after patches.Reset
+                return p
+            },
+            teardown: func() {
+                // restore SONIC_CLI_IFACE_MODE
+                os.Setenv(show_client.SonicCliIfaceMode, "")
+            },
+        },
+        {
+            desc:       "query SHOW interface switchport config - portchannel membership and colon-delimiter key",
+            pathTarget: "SHOW",
+            textPbPath: `
+                elem: <name: "interface" >
+                elem: <name: "switchport" >
+                elem: <name: "config" >
+            `,
+            wantRetCode: codes.OK,
+            valTest:     false,
+            testInit: func() {
+                FlushDataSet(t, ConfigDbNum)
+            },
+            mockPatch: func() *gomonkey.Patches {
+                // Provide deterministic table content for PORT / PORTCHANNEL / PORTCHANNEL_MEMBER / VLAN_MEMBER
+                return gomonkey.ApplyFunc(show_client.GetMapFromQueries, func(queries [][]string) (map[string]interface{}, error) {
+                    if len(queries) == 0 || len(queries[0]) < 2 {
+                        return map[string]interface{}{}, nil
+                    }
+                    table := queries[0][1]
+                    switch table {
+                    case "PORT":
+                        return map[string]interface{}{
+                            "Ethernet0": map[string]string{"alias": "etp0"},
+                            "Ethernet2": map[string]string{"alias": "etp2"},
+                            "Ethernet6": map[string]string{"alias": "etp6"},
+                        }, nil
+                    case "PORTCHANNEL":
+                        return map[string]interface{}{
+                            "PortChannel1": map[string]string{"mode": "trunk"},
+                        }, nil
+                    case "PORTCHANNEL_MEMBER":
+                        // use colon delimiter in member key
+                        return map[string]interface{}{
+                            "PortChannel1:Ethernet2": map[string]string{},
+                        }, nil
+                    case "VLAN_MEMBER":
+                        // include both '|' style and ':' style keys (the code's SplitCompositeKey supports both)
+                        return map[string]interface{}{
+                            "Vlan100|Ethernet0":   map[string]string{"tagging_mode": "untagged"},
+                            "Vlan2000:Ethernet6":  map[string]string{"tagging_mode": "tagged"},
+                        }, nil
+                    default:
+                        return map[string]interface{}{}, nil
+                    }
+                })
+            },
+        },
     }
 
     for _, test := range tests {
         if test.testInit != nil {
             test.testInit()
         }
-        var patches *gomonkey.Patches
+        var patchesSlice []*gomonkey.Patches
         if test.mockSleep {
-            patches = gomonkey.ApplyFunc(time.Sleep, func(d time.Duration) {})
+            patchesSlice = append(patchesSlice, gomonkey.ApplyFunc(time.Sleep, func(d time.Duration) {}))
+        }
+        if test.mockPatch != nil {
+            p := test.mockPatch()
+            if p != nil {
+                patchesSlice = append(patchesSlice, p)
+            }
         }
 
         t.Run(test.desc, func(t *testing.T) {
             runTestGet(t, ctx, gClient, test.pathTarget, test.textPbPath, test.wantRetCode, test.wantRespVal, test.valTest)
         })
-        if patches != nil {
-            patches.Reset()
+
+        for _, p := range patchesSlice {
+            if p != nil {
+                p.Reset()
+            }
+        }
+        if test.teardown != nil {
+            test.teardown()
         }
     }
 }
@@ -139,7 +263,7 @@ func TestGetShowInterfaceSwitchportStatus(t *testing.T) {
     portsFileName := "../testdata/PORTS_SWITCHPORT.txt"
     vlanMemberFileName := "../testdata/VLAN_MEMBER_SWITCHPORT.txt"
 
-    expectedStatus := `[{"Interface":"Ethernet0","Mode":"trunk"},{"Interface":"Ethernet1","Mode":"trunk"},{"Interface":"Ethernet2","Mode":"trunk"},{"Interface":"Ethernet3","Mode":"trunk"},{"Interface":"Ethernet4","Mode":"trunk"},{"Interface":"Ethernet5","Mode":"trunk"},{"Interface":"Ethernet6","Mode":"routed"},{"Interface":"Ethernet7","Mode":"routed"}]`
+    expectedStatus := `[{"Interface":"Ethernet0","Mode":"trunk"},{"Interface":"Ethernet1","Mode":"trunk"},{"Interface":"Ethernet2","Mode":"trunk"},{"Interface":"Ethernet3","Mode":"trunk"},{"Interface":"Ethernet4","Mode":"trunk"},{"Interface":"Ethernet5","Mode":"trunk"},{"Interface":"Ethernet6","Mode":"trunk"},{"Interface":"Ethernet7","Mode":"routed"}]`
 
     tests := []struct {
         desc        string
@@ -150,6 +274,8 @@ func TestGetShowInterfaceSwitchportStatus(t *testing.T) {
         valTest     bool
         mockSleep   bool
         testInit    func()
+        mockPatch   func() *gomonkey.Patches
+        teardown    func()
     }{
         {
             desc:       "query SHOW interface switchport status NO DATA",
@@ -194,22 +320,129 @@ func TestGetShowInterfaceSwitchportStatus(t *testing.T) {
             wantRespVal: []byte(`[{"Interface":"Ethernet0","Mode":"trunk"}]`),
             valTest:     true,
         },
+        // --------- new cases for status ----------
+        {
+            desc:       "query SHOW interface switchport status - GetMapFromQueries returns error",
+            pathTarget: "SHOW",
+            textPbPath: `
+                elem: <name: "interface" >
+                elem: <name: "switchport" >
+                elem: <name: "status" >
+            `,
+            wantRetCode: codes.NotFound,
+            valTest:     false,
+            testInit: func() {
+                FlushDataSet(t, ConfigDbNum)
+            },
+            mockPatch: func() *gomonkey.Patches {
+                return gomonkey.ApplyFunc(show_client.GetMapFromQueries, func(queries [][]string) (map[string]interface{}, error) {
+                    if len(queries) > 0 && len(queries[0]) > 1 && queries[0][1] == "PORT" {
+                        return nil, fmt.Errorf("injected failure")
+                    }
+                    return map[string]interface{}{}, nil
+                })
+            },
+        },
+        {
+            desc:       "query SHOW interface switchport status - alias display (SONIC_CLI_IFACE_MODE=alias)",
+            pathTarget: "SHOW",
+            textPbPath: `
+                elem: <name: "interface" >
+                elem: <name: "switchport" >
+                elem: <name: "status" >
+            `,
+            wantRetCode: codes.OK,
+            valTest:     false,
+            testInit: func() {
+                FlushDataSet(t, ConfigDbNum)
+                AddDataSet(t, ConfigDbNum, portsFileName)
+                AddDataSet(t, ConfigDbNum, vlanMemberFileName)
+            },
+            mockPatch: func() *gomonkey.Patches {
+                old := os.Getenv(show_client.SonicCliIfaceMode)
+                os.Setenv(show_client.SonicCliIfaceMode, "alias")
+                p := gomonkey.ApplyFunc(sdc.PortToAliasNameMap, func() map[string]string {
+                    return map[string]string{"Ethernet0": "etp0"}
+                })
+                // teardown will restore env
+                _ = old
+                return p
+            },
+            teardown: func() {
+                os.Setenv(show_client.SonicCliIfaceMode, "")
+            },
+        },
+        {
+            desc:       "query SHOW interface switchport status - portchannel membership and colon-delimiter key",
+            pathTarget: "SHOW",
+            textPbPath: `
+                elem: <name: "interface" >
+                elem: <name: "switchport" >
+                elem: <name: "status" >
+            `,
+            wantRetCode: codes.OK,
+            valTest:     false,
+            testInit: func() {
+                FlushDataSet(t, ConfigDbNum)
+            },
+            mockPatch: func() *gomonkey.Patches {
+                return gomonkey.ApplyFunc(show_client.GetMapFromQueries, func(queries [][]string) (map[string]interface{}, error) {
+                    if len(queries) == 0 || len(queries[0]) < 2 {
+                        return map[string]interface{}{}, nil
+                    }
+                    table := queries[0][1]
+                    switch table {
+                    case "PORT":
+                        return map[string]interface{}{
+                            "Ethernet0": map[string]string{"alias": "etp0"},
+                            "Ethernet2": map[string]string{"alias": "etp2"},
+                        }, nil
+                    case "PORTCHANNEL":
+                        return map[string]interface{}{
+                            "PortChannel1": map[string]string{"mode": "trunk"},
+                        }, nil
+                    case "PORTCHANNEL_MEMBER":
+                        return map[string]interface{}{
+                            "PortChannel1:Ethernet2": map[string]string{},
+                        }, nil
+                    case "VLAN_MEMBER":
+                        return map[string]interface{}{
+                            "Vlan100|Ethernet0": map[string]string{"tagging_mode": "untagged"},
+                        }, nil
+                    default:
+                        return map[string]interface{}{}, nil
+                    }
+                })
+            },
+        },
     }
 
     for _, test := range tests {
         if test.testInit != nil {
             test.testInit()
         }
-        var patches *gomonkey.Patches
+        var patchesSlice []*gomonkey.Patches
         if test.mockSleep {
-            patches = gomonkey.ApplyFunc(time.Sleep, func(d time.Duration) {})
+            patchesSlice = append(patchesSlice, gomonkey.ApplyFunc(time.Sleep, func(d time.Duration) {}))
+        }
+        if test.mockPatch != nil {
+            p := test.mockPatch()
+            if p != nil {
+                patchesSlice = append(patchesSlice, p)
+            }
         }
 
         t.Run(test.desc, func(t *testing.T) {
             runTestGet(t, ctx, gClient, test.pathTarget, test.textPbPath, test.wantRetCode, test.wantRespVal, test.valTest)
         })
-        if patches != nil {
-            patches.Reset()
+
+        for _, p := range patchesSlice {
+            if p != nil {
+                p.Reset()
+            }
+        }
+        if test.teardown != nil {
+            test.teardown()
         }
     }
 }
